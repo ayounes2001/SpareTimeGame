ca3e2b1ce7e7d972226b960ddac18c1c6b7d089b
Anthony Younes <1016744@student.sae.edu.au>
2020-02-06 21:17:24 +1100

    My Game Progress So far

---
+++ Brackey's Box Game/Assets/Prefabs/GameManager.cs
@@ -0,0 +1,33 @@
+﻿
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class GameManager : MonoBehaviour
+{
+    bool gameHasEnded = false;
+    float restartDelay = 2f;
+    public GameObject completeLevelUI;
+
+    public void CompleteLevel()
+    {
+        completeLevelUI.SetActive(true);
+    }
+    public void GameOver()
+    {
+        if(gameHasEnded == false)
+        {
+            gameHasEnded = true;
+            Debug.Log("Game Over");
+            //restart game
+            Invoke("Restart",restartDelay);
+
+        }
+
+    }
+    void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+} 
+
+    
+
---
+++ Brackey's Box Game/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    //reference to the RigidBody component as 'rb'
+    public Rigidbody rb;
+    public float forwardforce = 2000;
+    public float sidewaysForce = 500;
+    public float jumpSpeed = 250;
+
+    public GameObject leftButton, rightButton;
+   
+
+    bool OnGround = true;
+
+
+   
+    // Update is called once per frame
+    void FixedUpdate() //for physics stuff use FixUpdate()
+    {
+       
+        //add a forward force
+        rb.AddForce(0, 0, forwardforce * Time.deltaTime); //add a froce of 2000 on the Z axis
+
+        if(Input.GetKey("d"))
+        {
+            //addforce to right
+            rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0,ForceMode.VelocityChange);
+        }
+        if (Input.GetKey("a"))
+        {
+            //add force to left
+            rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+
+        if (rb.position.y < -1f)
+        {
+            FindObjectOfType <GameManager>().GameOver();
+        }
+        if(OnGround)
+        {
+            if(Input.GetKey("w"))
+            {
+                rb.velocity = new Vector3(0, 7f, jumpSpeed* Time.deltaTime);
+                OnGround = false;
+                
+
+            }
+        }
+        
+    }
+   
+}
